82e5fb04e3c04cca598ae6535fb62fda50bc77db
ergest2000 <topciuergest@gmail.com>
2026-03-22 19:40:45 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/dental-harmony-suite/",
+  base: "/crmdent/",
   server: {
     host: "::",
     port: 8080,
